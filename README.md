bfcd24fe0c2f47300dddfce7050fc59643a1fbff
mathieuhuet <mathieuhuet@live.ca>
2023-09-10 05:15:29 -0400

    Updated README with screenshots

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # GPMM_Alertes web version for desktop
 
 This version of GPMM_Alertes let you turn alerts and acquitment into PDF files, it comes handy when you need to share a specific event that happenened through email for example.
+
+
+## Screenshots
+
+![](GPMM_Desktop_01.png) 
+
+![](GPMM_Desktop_02.png) 
+
+![](GPMM_Desktop_03.png)
--- src/sercret.js
+++ src/sercret.js
@@ -1,3 +1,3 @@
-export const GPMM_USER_API = "http://192.168.71.88:10101/user";
-export const GPMM_ACTIVITY_API = "http://192.168.71.88:10101/activity";
-export const GPMM_SITE_API = "http://192.168.71.88:10101/site";
+export const GPMM_USER_API = "http://172.24.15.98:10101/user";
+export const GPMM_ACTIVITY_API = "http://172.24.15.98:10101/activity";
+export const GPMM_SITE_API = "http://172.24.15.98:10101/site";
